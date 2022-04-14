4517b17188e81c3642852706a3e7d7c69d572518
Your Name <you@example.com>
2022-04-14 02:01:26 -0700

    main

--- LinearRegression.py
+++ LinearRegression.py
@@ -3,5 +3,14 @@ import pandas
 import numpy
 
 # Doc File CSV
-data_linear = pandas.read_csv('data_linear.csv')
-print(data_linear)
+data = pandas.read_csv('data_linear.csv').values
+
+
+
+N = data.shape[0]
+x = data[:, 0].reshape(-1, 1)
+y = data[:, 1].reshape(-1, 1)
+
+print(N)
+print(x)
+print(y)
