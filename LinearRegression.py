71ca51b48f1199bf91ac067d70b6b3d22fa27cc5
Your Name <you@example.com>
2022-04-14 05:36:06 -0700

    main

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,75 +1,46 @@
-import matplotlib.pyplot as plt
-import pandas
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 22:06:34 2019
+
+@author: DELL
+"""
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#numOfPoint = 30
+#noise = np.random.normal(0,1,numOfPoint).reshape(-1,1)
+#x = np.linspace(30, 100, numOfPoint).reshape(-1,1)
+#N = x.shape[0]
+#y = 15*x + 8 + 20*noise
+#plt.scatter(x, y)
 
+data = pd.read_csv('data_linear.csv').values
+N = data.shape[0]
+x = data[:, 0].reshape(-1, 1)
+y = data[:, 1].reshape(-1, 1)
+plt.scatter(x, y)
+plt.xlabel('mét vuông')
+plt.ylabel('giá')
 
-# Doc File CSV
-data = pandas.read_csv('data_linear.csv').values
+x = np.hstack((np.ones((N, 1)), x))
 
-x = data[:,0].reshape(-1,1)
-''' [[ 30.    ]
- [ 32.4138]
- [ 34.8276]
- [ 37.2414]
- [ 39.6552]
- [ 42.069 ]
- [ 44.4828]
- [ 46.8966]
- [ 49.3103]
- [ 51.7241]
- [ 54.1379]
- [ 56.5517]
- [ 58.9655]
- [ 61.3793]
- [ 63.7931]
- [ 66.2069]
- [ 68.6207]
- [ 71.0345]
- [ 73.4483]
- [ 75.8621]
- [ 78.2759]
- [ 80.6897]
- [ 83.1034]
- [ 85.5172]
- [ 87.931 ]
- [ 90.3448]
- [ 92.7586]
- [ 95.1724]
- [ 97.5862]
- [100.    ]] '''
- 
-y = data[:,1].reshape(-1,1)
-''' [[ 448.524]
- [ 509.248]
- [ 535.104]
- [ 551.432]
- [ 623.418]
- [ 625.992]
- [ 655.248]
- [ 701.377]
- [ 748.918]
- [ 757.881]
- [ 831.004]
- [ 855.409]
- [ 866.707]
- [ 902.545]
- [ 952.261]
- [ 995.531]
- [1069.78 ]
- [1074.42 ]
- [1103.88 ]
- [1138.69 ]
- [1153.13 ]
- [1240.27 ]
- [1251.9  ]
- [1287.97 ]
- [1320.47 ]
- [1374.92 ]
- [1410.16 ]
- [1469.69 ]
- [1478.54 ]
- [1515.28 ]] '''
- 
-w = np.array(([1],[0]))
-''' [[1] 
- [0]] '''
+w = np.array([0.,1.]).reshape(-1,1)
+
+numOfIteration = 1000
+cost = np.zeros((numOfIteration,1))
+learning_rate = 0.0000001
+for i in range(1, numOfIteration):
+    r = np.dot(x, w) - y
+    cost[i] = 0.5*np.sum(r*r)
+    w[0] -= learning_rate*np.sum(r)
+    # correct the shape dimension
+    w[1] -= learning_rate*np.sum(np.multiply(r, x[:,1].reshape(-1,1)))
+    print(cost[i])
+predict = np.dot(x, w)
+plt.plot((x[0][1], x[N-1][1]),(predict[0], predict[N-1]), 'r')
+plt.show()
+
+x1 = 50
+y1 = w[0] + w[1] * 50
+print('Giá nhà cho 50m^2 là : ', y1)
