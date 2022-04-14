80dbd7c5bfd2711faabc2420d02548d29124249d
Your Name <you@example.com>
2022-04-14 02:45:04 -0700

    main

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas
-import numpy
+import numpy as np
 
 # Doc File CSV
 data = pandas.read_csv('data_linear.csv').values
@@ -15,3 +15,34 @@ y = data[:,1]
 
 print(x)
 print(y)
+
+# Doi thanh matran cot
+x = x.reshape(-1,1)
+y = y.reshape(-1,1)
+
+print(x)
+print(y)
+
+
+# Ve do thi bieu dien gia tri
+plt.scatter(x, y)
+plt.title('Giá nhà theo mét vuông')
+plt.xlabel('mét vuông')
+plt.ylabel('giá')
+#plt.show()
+
+# Thêm cột giá trị = 1 vào matran
+x = np.hstack((np.ones((N, 1)), x))
+
+print(x)
+
+# Khởi tạo matran W
+
+w = np.array([[0],[1]])
+print(w)
+
+# Khoi tap gia tri du doan voi Yi = x*w
+Yi = np.dot(x,w)
+print(Yi)
+
+print(Yi-y)
