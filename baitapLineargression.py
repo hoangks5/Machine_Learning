42be36cc497490902c1bd03d5814a4ffa49299e1
Your Name <you@example.com>
2022-04-16 05:29:45 -0700

    bài tập 5 ẩn linerregression

--- baitapLineargression.py
+++ baitapLineargression.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 
 data = np.array([
         [1, 100, 7, 42, 0], [1, 150, 7, 62, 0], [1, 118, 6, 40, 0], [1, 123, 6, 54, 0], [1, 112, 6, 42, 0],
@@ -20,7 +21,36 @@ x4 = data[:,4].reshape(-1,1)
 y = np.array([259, 295, 279, 259, 299, 299, 309, 289, 849, 829, 359, 315, 310, 309, 300, 289, 369, 419, 405, 439]).reshape(-1,1)
 
 
-w = np.array([[0],[1],[1],[1],[1]])
-r = np.dot(data,w)-y
-print(r)
-# r = Yi - y
+
+
+
+number = 100000
+
+learning_rate = 0.000002
+cost = np.zeros((number,1))
+w = [[1],[1],[1],[1],[1]]
+''' for i in range(number):
+      r = np.dot(data,w)-y
+      cost[i] = 0.5*np.sum(r*r)
+      print(cost[i])
+      w[0] = w[0] - learning_rate*np.sum(r)
+      w[1] = w[1] - learning_rate*np.sum(np.multiply(r,data[:,1].reshape(-1,1)))
+      w[2] = w[2] - learning_rate*np.sum(np.multiply(r,data[:,2].reshape(-1,1)))
+      w[3] = w[3] - learning_rate*np.sum(np.multiply(r,data[:,3].reshape(-1,1)))
+      w[4] = w[4] - learning_rate*np.sum(np.multiply(r,data[:,4].reshape(-1,1)))
+      #print(np.array(w)) '''
+      
+
+w = [ 7.42113182,
+  1.86950514,
+ 25.57630942,
+ -2.27245749,
+ 22.0105757 ]
+
+while True:
+      xa = int(input('Diện tích: '))
+      xb = int(input('Số phòng: '))
+      xc = int(input('Tuổi: '))
+      xd = int(input('Bếp: '))
+      tien = w[0]+xa*w[1]+xb*w[2]+xc*w[3]+xd*w[4]
+      print(tien)
